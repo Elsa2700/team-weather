71240f5fc2845c81b74852c48c916d69c6e9e737
Ko <orangeko@gmail.com>
2021-06-09 20:53:40 +0900

    ready to adjust arrange from north to south

--- js/menu.js
+++ js/menu.js
@@ -45,24 +45,85 @@ c. "elementValue": [{"value": "多雲。降雨機率 10%。...}] (我找到概�
 menuFrameController()
 function menuFrameController(){
     renderHTML()
-	// getData()
+	getData()
 }
 
 
 function renderHTML(){
-    document.getElementsByClassName('menu')[0].innerHTML = '<div id="menu-frame"></div>'
-    let menu_frame = document.querySelector('#menu-frame');
+    // let description = document.createElement('div')
+    // description.textContent = "各地近12小時天氣預報"
+    // let class_menu = document.querySelector('.menu')
+    // class_menu.append(description)
+    document.getElementsByClassName('menu')[0].innerHTML = 
+    '<div class="description">各地近3小時天氣預報</div><div id="menu-frame"></div>';
+    let id_menu_frame = document.querySelector('#menu-frame');
     
     
-    for (let i = 0; i < 21; i++){
-        let box_area = document.createElement('div');
-        box_area.setAttribute('class', 'administrative_district')
-        menu_frame.append(box_area)
-        console.log(i)
+    
+    for (let i = 1; i <= 21; i++){
+        let box_location = document.createElement('div');
+        box_location.setAttribute('id', 'box_no_' +i);
+        box_location.setAttribute('class', 'administrative_district');
+        let location_name = document.createElement('div')
+        location_name.setAttribute('class', 'location_name')
+        let location_whether = document.createElement('img')
+        location_whether.setAttribute('class', 'location_whether')
+        
+        let raining_rate = document.createElement('div');
+        raining_rate.setAttribute('class', 'location_raining_rate');
+        
+        box_location.append(location_name, location_whether, raining_rate)
+
+        id_menu_frame.append(box_location);
     }
 }
 
 
 function getData(){
+    api_url = "https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-089?Authorization=" + CWB_API_KEY
+
+    fetch(api_url).then( res => {
+        return res.json()
+    }).then( data => {
+        // 圖在這裡
+		// https://www.cwb.gov.tw/V8/assets/img/weather_icons/weathers/svg_icon/day/01.svg
+		let svgurl = 'https://www.cwb.gov.tw/V8/assets/img/weather_icons/weathers/svg_icon/day/'
+        location_records = data.records.locations[0] //全台資料
+        let all_laptitude ={};
+        for (let i = 1; i <= 21; i++){
+            let location_name = location_records['location'][i]['locationName'];
+            let id_box_no = '#box_no_'+i;
+            let box_location_no = document.querySelector(id_box_no)
+            let class_location_name = box_location_no.querySelector('.location_name')
+            class_location_name.append(location_name)
+            
+
+            let location_whether_code = location_records['location'][i]['weatherElement'][1]['time'][0]['elementValue'][1]['value']
+            
+            let class_location_whether = box_location_no.querySelector('.location_whether')
+            
+            class_location_whether.setAttribute('src', svgurl + location_whether_code + '.svg')
+            
+            
+            let location_raining_rate = location_records['location'][i]['weatherElement'][7]['time'][1]['elementValue'][0]['value']
+            let class_location_raining_rate = box_location_no.querySelector('.location_raining_rate')
+            class_location_raining_rate.append('降雨率 ' + location_raining_rate +'%')
+            
+            let location_temp = location_records['location'][i]['weatherElement'][3]['time'][0]['elementValue'][0]['value']
+            let location_temp_body = location_records['location'][i]['weatherElement'][2]['time'][0]['elementValue'][0]['value']
+            // console.log("🚀 ~ file: menu.js ~ line 114 ~ fetch ~ location_temp_body", location_temp_body)
+            // console.log("🚀 ~ file: menu.js ~ line 92 ~ fetch ~ location_whether", location_temp)
+
+            let laptitude = location_records['location'][i]['lat']
+            all_laptitude[location_name] = parseFloat(laptitude)
+            
+        }
+
+        location_sorted_by_laptitude = Object.keys(all_laptitude).sort(function(a,b){return all_laptitude[b]-all_laptitude[a]})
+        console.log("🚀 ~ file: menu.js ~ line 124 ~ fetch ~ location_sorted_by_laptitude", location_sorted_by_laptitude)
+        
+
+
+    })
 
 }
