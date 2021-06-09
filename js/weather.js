9139ee40012aea191c08776c5a9e7a676bd84300
elsa <elsa2700@gmail.com>
2021-06-10 00:05:24 +0800

    會議結論

--- js/weather.js
+++ js/weather.js
@@ -1,8 +1,9 @@
 /*
 【注意事項】
 1.基本串接對象
+url = 'https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-089?Authorization=CWB-2EF6C203-2256-404D-AD80-5E9DE0982C6A'
 API: https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-089臺灣各鄉鎮市區預報資料-臺灣各鄉鎮市區未來2天(逐3小時)
-項目: 
+項目:
 a. "elementValue": [{"value": "晴",...}..],(天氣狀況，建議用陰、晴兩種圖片來顯示，可跟menu中顯示圖片統一表示或其他呈現)
 b. "elementName": "T", "description": "溫度",
 c. "elementName": "PoP12h","description": "12小時降雨機率",
@@ -42,3 +43,166 @@ function renderRaining(page){
 	}
 }
 */
+
+
+
+weatherFrameController()
+function weatherFrameController() {
+	renderHTML()
+	getData()
+}
+
+function renderHTML() {
+	document.getElementsByClassName('weather')[0].innerHTML = '<div id="weather-frame"></div>'
+	let weatherFrameHTML = `
+        <div class="fourBox">
+          <div class="box">
+            <div class="startTime"></div>
+            <div class="boxHead"></div>
+            <!-- <div class="boxBody"> -->
+            <div class="WXimg"></div>
+            <div class="WXtxt"></div>
+            <!-- </div> -->
+          </div>
+          <div class="box">
+            <div class="startTime"></div>
+            <div class="boxHead"></div>
+            <!-- <div class="boxBody"> -->
+            <div class="WXimg"></div>
+            <div class="WXtxt"></div>
+            <!-- </div> -->
+          </div>
+          <div class="box">
+            <div class="startTime"></div>
+            <div class="boxHead"></div>
+            <!-- <div class="boxBody"> -->
+            <div class="WXimg"></div>
+            <div class="WXtxt"></div>
+            <!-- </div> -->
+          </div>
+          <div class="box">
+            <div class="startTime"></div>
+            <div class="boxHead"></div>
+            <!-- <div class="boxBody"> -->
+            <div class="WXimg"></div>
+            <div class="WXtxt"></div>
+            <!-- </div> -->
+          </div>
+        </div>
+	`
+	document.getElementById('weather-frame').innerHTML = (weatherFrameHTML)
+}
+
+let records = null
+let locationName = '臺北市'
+
+function getData() {
+	fetch("https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-091?Authorization=" + CWB_API_KEY).then((response) => {
+		return response.json()
+	}).then((data) => {
+		records = data.records
+		taipeiLocation = records.locations[0].location
+		// 圖在這裡
+		// https://www.cwb.gov.tw/V8/assets/img/weather_icons/weathers/svg_icon/day/01.svg
+		let svgurl = 'https://www.cwb.gov.tw/V8/assets/img/weather_icons/weathers/svg_icon/day/'
+		for (let cityIndex = 0; cityIndex < taipeiLocation.length; cityIndex++) {
+			if (taipeiLocation[cityIndex].locationName === locationName) {
+				// console.log(taipeiLocation[cityIndex].weatherElement)
+				let weatherWX = taipeiLocation[cityIndex].weatherElement[6]
+				let weatherPoP = taipeiLocation[cityIndex].weatherElement[0]
+				let weatherMinT = taipeiLocation[cityIndex].weatherElement[8]
+				let weatherMaxT = taipeiLocation[cityIndex].weatherElement[12]
+				let weatherMinAT = taipeiLocation[cityIndex].weatherElement[11]
+				let weatherMaxAT = taipeiLocation[cityIndex].weatherElement[5]
+				let startTime, endTime, WXtxt, svgNumber, WXimg, PoPtxt
+				for (let boxIndex = 0; boxIndex < 4; boxIndex++) {
+					// 顯示 start time
+					startTime = weatherWX.time[boxIndex].startTime
+					endTime = weatherWX.time[boxIndex].endTime
+					document.getElementsByClassName('startTime')[boxIndex].innerHTML = startTime.split(' ')[0].substring(5, 10).replace('-', '/')
+					document.getElementsByClassName('boxHead')[boxIndex].innerHTML = String(startTime.split(' ')[1].substring(0, 2)) + '-' + (endTime.split(' ')[1].substring(0, 2))
+
+					// 白天or晚上
+
+					if (String(startTime.substring(11, 13)) === '18') {
+						document.getElementsByClassName('boxHead')[boxIndex].innerHTML = '晚上'
+						document.getElementsByClassName('box')[boxIndex].setAttribute('class', 'box night')
+					} else if (String(startTime.substring(11, 13)) === '12') {
+						document.getElementsByClassName('boxHead')[boxIndex].innerHTML = '下午'
+						document.getElementsByClassName('box')[boxIndex].setAttribute('class', 'box afternoon')
+					} else if (String(startTime.substring(11, 13)) === '00') {
+						document.getElementsByClassName('boxHead')[boxIndex].innerHTML = '凌晨'
+						document.getElementsByClassName('box')[boxIndex].setAttribute('class', 'box dawn')
+					} else {
+						document.getElementsByClassName('boxHead')[boxIndex].innerHTML = '白天'
+						document.getElementsByClassName('box')[boxIndex].setAttribute('class', 'box day')
+					}
+					// WX:天氣現象
+					WXtxt = weatherWX.time[boxIndex].elementValue[0].value
+					svgNumber = weatherWX.time[boxIndex].elementValue[1].value
+					WXimg = document.createElement('img')
+					WXimg.setAttribute('src', svgurl + svgNumber + '.svg')
+					document.getElementsByClassName('WXimg')[boxIndex].appendChild(WXimg)
+					document.getElementsByClassName('WXtxt')[boxIndex].innerHTML = WXtxt
+
+					// PoP12h: 降雨機率
+					let PoP = document.createElement('div')
+					PoP.setAttribute('class', 'PoPdiv')
+					document.getElementsByClassName('box')[boxIndex].appendChild(PoP)
+
+					let PoPumbrella = document.createElement('img')
+					PoPumbrella.setAttribute('class', 'umbrella')
+					PoPumbrella.setAttribute('src', './img/umbrella.png')
+					document.getElementsByClassName('PoPdiv')[boxIndex].appendChild(PoPumbrella)
+
+					PoPtxt = weatherPoP.time[boxIndex].elementValue[0].value
+					let PoPdiv = document.createElement('div')
+					PoPdiv.appendChild(document.createTextNode(PoPtxt + '%'))
+					PoPdiv.setAttribute('class', 'PoPtxt')
+					document.getElementsByClassName('PoPdiv')[boxIndex].appendChild(PoPdiv)
+
+					// MinT 最低溫度 MaxT 最高溫度
+					let T = document.createElement('div')
+					T.setAttribute('class', 'Tdiv')
+					document.getElementsByClassName('box')[boxIndex].appendChild(T)
+
+					let Ttemperature = document.createElement('img')
+					Ttemperature.setAttribute('class', 'umbrella')
+					Ttemperature.setAttribute('src', './img/temperature.png')
+					document.getElementsByClassName('Tdiv')[boxIndex].appendChild(Ttemperature)
+
+					let Ttxt = (weatherMinT.time[boxIndex].elementValue[0].value) + '-' + (weatherMaxT.time[boxIndex].elementValue[0].value)
+					let Tdiv = document.createElement('div')
+					Tdiv.appendChild(document.createTextNode(Ttxt))
+					document.getElementsByClassName('Tdiv')[boxIndex].appendChild(Tdiv)
+
+					// 體感溫度
+					let AT = document.createElement('div')
+					AT.setAttribute('class', 'ATdiv')
+					document.getElementsByClassName('box')[boxIndex].appendChild(AT)
+
+					let ATtemperature = document.createElement('img')
+					ATtemperature.setAttribute('class', 'feeltemp')
+					ATtemperature.setAttribute('src', './img/feel.png')
+					ATtemperature.setAttribute('title', '體感溫度')
+					document.getElementsByClassName('ATdiv')[boxIndex].appendChild(ATtemperature)
+
+					let ATtxt = (weatherMinAT.time[boxIndex].elementValue[0].value) + '-' + (weatherMaxAT.time[boxIndex].elementValue[0].value)
+					let ATdiv = document.createElement('div')
+					ATdiv.appendChild(document.createTextNode(ATtxt))
+					document.getElementsByClassName('ATdiv')[boxIndex].appendChild(ATdiv)
+
+
+				}
+			}
+		}
+	})
+}
+
+
+
+
+
+
+
+
